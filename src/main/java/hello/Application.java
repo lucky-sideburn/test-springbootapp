8c5708022afa956ac31e15d804cf2248446437ba
Eugenio Marzo <eugenio@em-2.local>
2018-08-17 22:29:50 +0200

    pulsano

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World! And CI for Openshift! ferie!";
+        return "Hello Docker World! And CI for Openshift! welcome to pulsano";
     }
 
     public static void main(String[] args) {
